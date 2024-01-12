9acc9a444c78c3edd9c58fd967e40aae33f461fd
Sergiws <sergiws967@gmail.com>
2024-01-12 20:07:06 +0100

    Crear un proyecto java

---
+++ src/main/java/com/sergiorocasaez/practicatienda/Practicatienda.java
@@ -0,0 +1,16 @@
+/*
+ * Inventario de Tienda: Escribe un programa para gestionar el inventario de una
+ * tienda utilizando ArrayList. Debe permitir agregar productos con detalles como
+ * nombre, precio y cantidad en stock, así como realizar operaciones como actualizar
+ * inventario, buscar productos y mostrar el inventario actualizado.
+ */
+
+package com.sergiorocasaez.practicatienda;
+
+public class Practicatienda {
+
+    public static void main(String[] args) {
+        System.out.println("Si lees esto, significa que la prueba de GitHub ha salido bien");
+        System.out.println("El próximo push será la práctica hecha");
+    }
+}
